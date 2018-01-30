a00afe5f418d3f79a55a8dbc9f36f330dad574f7
Narendra <narry051@gmail.com>
2018-01-30 10:25:32 +0530

    asdasd
    asdasd

--- application/views/admin/nav_admin.php
+++ application/views/admin/nav_admin.php
@@ -52,7 +52,7 @@
                             </div>
                         </div>
                         <div class="box-body">
-                            <table id="example1" class="table table-bordered table-striped">
+                            <table id="example1" class="table table-bordered">
                                 <thead>
                                     <tr>
                                         <th>Name</th>
